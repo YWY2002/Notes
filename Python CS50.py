0a05e063cffe08a511403ed56a349e361ed9fc9f
YWY2002 <97068401+YWY2002@users.noreply.github.com>
2023-06-30 21:46:24 +0800

    Update and rename Python CS50 to Python CS50.py

--- Python CS50.py
+++ Python CS50.py
@@ -1,38 +1,36 @@
-#CS50 Notes
-
-#name=input("What is your name? ").strip().title()
+name=input("What is your name? ").strip().title()
 
 #Remove whitespaces and capitalise first letter of each word
-#name=name.strip().title()
+name=name.strip().title()
 
 #Capitalize first word
-#name=name.capitalize()
+name=name.capitalize()
 
 #Split user's name into first name and last name
-#first, last = name.split(" ")
-#print("Your name is", name, end="\n")
-#print(f"Your name is {first}")
+first, last = name.split(" ")
+print("Your name is", name, end="\n")
+print(f"Your name is {first}")
 
 #------------------------------------------------------------------#
 
-#x=float(input("What is X? "))
-#y=float(input("What is Y? "))
-#z=round(x/y, 2)     #round to 2 decimal place
+x=float(input("What is X? "))
+y=float(input("What is Y? "))
+z=round(x/y, 2)     #round to 2 decimal place
 
-#print(f"{z:,}")     #put , in long numbers
-#print(f"{z:.2f}")   #round off to 2 decimal
+print(f"{z:,}")     #put , in long numbers
+print(f"{z:.2f}")   #round off to 2 decimal
 
 #------------------------------------------------------------------#
-"""
+
 def hello(to="world"):
     print("Hello,", to)
 
 hello()
 name=input("What is your name? ")
 hello(name)
-"""
+
 #------------------------------------------------------------------#
-"""
+
 def main():
     x=int(input("What is x? "))
     print("x squared is" , square(x))
@@ -41,4 +39,6 @@ def square(n):
     return pow(n, 2)     #power of 2
 
 main()
-"""
+
+#------------------------------------------------------------------#
+
