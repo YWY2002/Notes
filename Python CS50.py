d1042a709d09cb789237efc1b50f6b016b7b3eca
YWY2002 <97068401+YWY2002@users.noreply.github.com>
2023-08-02 13:50:48 +0800

    Update Python CS50.py

--- Python CS50.py
+++ Python CS50.py
@@ -61,3 +61,11 @@ match name:
         print("Who?")
 
 #------------------------------------------------------------------#
+
+while True:
+    n = int(input("What is n? "))
+    if n >=0:
+        break
+
+for _ in range(n):
+    print("meow")
