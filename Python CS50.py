e5d8612a6a0a5f6c2d3e6a1bad2c6dddb9bb1db8
YWY2002 <97068401+YWY2002@users.noreply.github.com>
2023-11-05 21:12:41 +0800

    Update Python CS50.py

--- Python CS50.py
+++ Python CS50.py
@@ -109,3 +109,15 @@ for student in students:
     print(students["name"], students["house"], sep=",")
 
 #------------------------------------------------------------------#
+#Exceptions
+while True:
+    try:
+        x = int(input("What's x?"))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+
+print(f"x is {x}")
+
+#------------------------------------------------------------------#
